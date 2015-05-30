c885b2d7de3526ab5379bae95dd09bb8db353750
Henrik Heino <henrik.heino@gmail.com>
2015-05-30 21:14:22 +0200

    Rectangle can now use texture.

--- js/photosphere.js
+++ js/photosphere.js
@@ -1,4 +1,5 @@
 var gl;
+var shader_program;
 
 function init_gl(canvas)
 {
@@ -10,7 +11,7 @@ function init_gl(canvas)
 	catch (e) {
 	}
 	if (!gl) {
-		alert("Unable to initialize WebGL!");
+		alert('Unable to initialize WebGL!');
 	}
 }
 
@@ -32,23 +33,25 @@ function compile_shader(gl, code, type)
 function init_shaders()
 {
 	var vertex_shader_code =
-	"attribute vec2 vrt_pos;\n" +
-	"\n" +
-	"varying vec2 pos;\n" +
-	"\n" +
-	"void main(void) {\n" +
-	"	pos = vrt_pos;\n" +
-	"	gl_Position = vec4(vrt_pos, 0.0, 1.0);\n" +
-	"}\n";
+	'attribute vec2 vrt_pos;\n' +
+	'\n' +
+	'varying vec2 pos;\n' +
+	'\n' +
+	'void main(void) {\n' +
+	'	pos = vrt_pos;\n' +
+	'	gl_Position = vec4(vrt_pos, 0.0, 1.0);\n' +
+	'}\n';
 
 	var fragment_shader_code =
-	"precision mediump float;\n" +
-	"\n" +
-	"varying vec2 pos;\n" +
-	"\n" +
-	"void main(void) {\n" +
-	"	gl_FragColor = vec4(0.5 + pos.x / 2.0, 0.5 + pos.y / 2.0, 1.0, 1.0);\n" +
-	"}\n";
+	'precision mediump float;\n' +
+	'\n' +
+	'uniform sampler2D tex;\n' +
+	'\n' +
+	'varying vec2 pos;\n' +
+	'\n' +
+	'void main(void) {\n' +
+	'	gl_FragColor = texture2D(tex, vec2(0.5 + pos.x / 2.0, 0.5 + pos.y / 2.0));\n' +
+	'}\n';
 
 	var vertex_shader = compile_shader(gl, vertex_shader_code, gl.VERTEX_SHADER);
 	var fragment_shader = compile_shader(gl, fragment_shader_code, gl.FRAGMENT_SHADER);
@@ -59,13 +62,15 @@ function init_shaders()
 	gl.linkProgram(shader_program);
 
 	if (!gl.getProgramParameter(shader_program, gl.LINK_STATUS)) {
-	    alert("Unable to link shaders!");
+		alert('Unable to link shaders!');
 	}
 
 	gl.useProgram(shader_program);
 
-	shader_program.vrt_pos_attr = gl.getAttribLocation(shader_program, "vrt_pos");
+	shader_program.vrt_pos_attr = gl.getAttribLocation(shader_program, 'vrt_pos');
 	gl.enableVertexAttribArray(shader_program.vrt_pos_attr);
+
+	shader_program.unif_tex = gl.getUniformLocation(shader_program, 'tex');
 
 	return shader_program;
 }
@@ -87,9 +92,34 @@ function init_buffers()
 	vrt_pos_buf.num_items = 4;
 }
 
-function draw_sphere(shader_program)
+var texture;
+function init_texture(image_src)
+{
+	texture = gl.createTexture();
+	texture.image = new Image();
+	texture.image.onload = function() {
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+		gl.bindTexture(gl.TEXTURE_2D, null);
+		texture.loaded = true;
+		draw_sphere();
+	}
+
+	texture.image.src = image_src;
+}
+
+function draw_sphere()
 {
 	gl.viewport(0, 0, gl.viewport_width, gl.viewport_height);
+
+	if (texture.loaded) {
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.uniform1i(shader_program.unif_tex, 0);
+	}
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, vrt_pos_buf);
 	gl.vertexAttribPointer(shader_program.vrt_pos_attr, vrt_pos_buf.item_size, gl.FLOAT, false, 0, 0);
@@ -108,10 +138,11 @@ function show_photosphere(canvas_id, image_src)
 	canvas.height = canvas.offsetHeight;
 
 	init_gl(canvas);
-	var shader_program = init_shaders();
+	shader_program = init_shaders();
 	init_buffers();
+	init_texture(image_src);
 
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-	draw_sphere(shader_program);
+	draw_sphere();
 }
