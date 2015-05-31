0ad2a3827b8c0e7355adb4c64a51381e65578856
Henrik Heino <henrik.heino@gmail.com>
2015-05-31 19:10:05 +0300

    Global variables removed. Photosphere is now class based.

--- js/photosphere.js
+++ js/photosphere.js
@@ -1,36 +1,73 @@
-var gl;
-var shader_program;
+var Photosphere = function(canvas_id, image_src)
+{
+	// Fov is calculated from top edge to bottom edge
+	this.camera_rotating = false;
+	this.camera_fov_y_degrees = 45;
+	this.camera_pitch = 0;
+	this.camera_yaw = 0;
+	this.mouse_last_pos_x = 0;
+	this.mouse_last_pos_y = 0;
 
-// Fov is calculated from top edge to bottom edge
-var camera_rotating = false;
-var camera_fov_y_degrees = 45;
-var camera_pitch = 0;
-var camera_yaw = 0;
-var mouse_last_pos_x = 0;
-var mouse_last_pos_y = 0;
+	this.canvas = document.getElementById(canvas_id);
+	if (!this.canvas) {
+		alert('Unable to find canvas with ID "' + canvas_id + '"!');
+		return;
+	}
 
-function init_gl(canvas)
+	this.canvas.width = this.canvas.offsetWidth;
+	this.canvas.height = this.canvas.offsetHeight;
+
+	// Listen mouse dragging
+	var photosphere = this;
+	this.canvas.addEventListener('mousedown', function(event) {
+		photosphere.mouse_event_down(event);
+	});	
+	this.canvas.addEventListener('mouseup', function(event) {
+		photosphere.mouse_event_up(event);
+	});
+	canvas.addEventListener('mousemove', function(event) {
+		photosphere.mouse_event_move(event);
+	});
+	// Listen mouse wheel
+	this.canvas.addEventListener('mousewheel', function(event) {
+		photosphere.mouse_event_wheel(event);
+	});	
+	this.canvas.addEventListener('DOMMouseScroll', function(event) {
+		photosphere.mouse_event_wheel(event);
+	});	
+
+	this.init_gl();
+	this.init_shaders();
+	this.init_buffers();
+	this.init_texture(image_src);
+
+	this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+	this.draw_sphere();
+}
+
+Photosphere.prototype.init_gl = function()
 {
 	try {
-		gl = canvas.getContext('experimental-webgl');
-		gl.viewport_width = canvas.width;
-		gl.viewport_height = canvas.height;
+		this.gl = this.canvas.getContext('experimental-webgl');
+		this.gl.viewport_width = this.canvas.width;
+		this.gl.viewport_height = this.canvas.height;
 	}
 	catch (e) {
 	}
-	if (!gl) {
+	if (!this.gl) {
 		alert('Unable to initialize WebGL!');
 	}
 }
 
-function compile_shader(gl, code, type)
+Photosphere.prototype.compile_shader = function(code, type)
 {
-	var shader = gl.createShader(type);
+	var shader = this.gl.createShader(type);
 
-	gl.shaderSource(shader, code);
-	gl.compileShader(shader);
+	this.gl.shaderSource(shader, code);
+	this.gl.compileShader(shader);
 
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+	if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
 		alert(gl.getShaderInfoLog(shader));
 		return null;
 	}
@@ -38,7 +75,7 @@ function compile_shader(gl, code, type)
 	return shader;
 }
 
-function init_shaders()
+Photosphere.prototype.init_shaders = function()
 {
 	var vertex_shader_code =
 	'attribute vec2 vrt_pos;\n' +
@@ -108,188 +145,170 @@ function init_shaders()
 	'	gl_FragColor = texture2D(tex, tex_pos);\n' +
 	'}\n';
 
-	var vertex_shader = compile_shader(gl, vertex_shader_code, gl.VERTEX_SHADER);
-	var fragment_shader = compile_shader(gl, fragment_shader_code, gl.FRAGMENT_SHADER);
+	var vertex_shader = this.compile_shader(vertex_shader_code, this.gl.VERTEX_SHADER);
+	var fragment_shader = this.compile_shader(fragment_shader_code, this.gl.FRAGMENT_SHADER);
 
-	var shader_program = gl.createProgram();
-	gl.attachShader(shader_program, fragment_shader);
-	gl.attachShader(shader_program, vertex_shader);
-	gl.linkProgram(shader_program);
+	this.shader_program = this.gl.createProgram();
+	this.gl.attachShader(this.shader_program, fragment_shader);
+	this.gl.attachShader(this.shader_program, vertex_shader);
+	this.gl.linkProgram(this.shader_program);
 
-	if (!gl.getProgramParameter(shader_program, gl.LINK_STATUS)) {
+	if (!this.gl.getProgramParameter(this.shader_program, this.gl.LINK_STATUS)) {
 		alert('Unable to link shaders!');
 	}
 
-	gl.useProgram(shader_program);
+	this.gl.useProgram(this.shader_program);
 
-	shader_program.vrt_pos_attr = gl.getAttribLocation(shader_program, 'vrt_pos');
-	gl.enableVertexAttribArray(shader_program.vrt_pos_attr);
+	this.shader_program.vrt_pos_attr = this.gl.getAttribLocation(this.shader_program, 'vrt_pos');
+	this.gl.enableVertexAttribArray(this.shader_program.vrt_pos_attr);
 
-	shader_program.unif_tex = gl.getUniformLocation(shader_program, 'tex');
-	shader_program.unif_fov_x = gl.getUniformLocation(shader_program, 'fov_x');
-	shader_program.unif_fov_y = gl.getUniformLocation(shader_program, 'fov_y');
-	shader_program.unif_camera_rot = gl.getUniformLocation(shader_program, 'camera_rot');
-
-	return shader_program;
+	this.shader_program.unif_tex = this.gl.getUniformLocation(this.shader_program, 'tex');
+	this.shader_program.unif_fov_x = this.gl.getUniformLocation(this.shader_program, 'fov_x');
+	this.shader_program.unif_fov_y = this.gl.getUniformLocation(this.shader_program, 'fov_y');
+	this.shader_program.unif_camera_rot = this.gl.getUniformLocation(this.shader_program, 'camera_rot');
 }
 
-var vrt_pos_buf;
-
-function init_buffers()
+Photosphere.prototype.init_buffers = function()
 {
-	vrt_pos_buf = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vrt_pos_buf);
+	this.vrt_pos_buf = this.gl.createBuffer();
+	this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vrt_pos_buf);
 	var vertices = [
 		 1, -1,
 		 1,  1,
 		-1, -1,
 		-1,  1
 	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	vrt_pos_buf.item_size = 2;
-	vrt_pos_buf.num_items = 4;
+	this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(vertices), this.gl.STATIC_DRAW);
+	this.vrt_pos_buf.item_size = 2;
+	this.vrt_pos_buf.num_items = 4;
 }
 
-var texture;
-function init_texture(image_src)
+Photosphere.prototype.init_texture = function(image_src)
 {
-	texture = gl.createTexture();
-	texture.image = new Image();
-	texture.image.onload = function() {
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-		// Clamp horizontally, but not vertically
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT); 
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-		gl.bindTexture(gl.TEXTURE_2D, null);
-		texture.loaded = true;
-		draw_sphere();
+	this.texture = this.gl.createTexture();
+	this.texture.image = new Image();
+	var photosphere = this;
+	this.texture.image.onload = function() {
+		photosphere.image_loaded();
 	}
-
-	texture.image.src = image_src;
+	this.texture.image.src = image_src;
 }
 
-function draw_sphere()
+Photosphere.prototype.image_loaded = function()
 {
-	gl.viewport(0, 0, gl.viewport_width, gl.viewport_height);
+	this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+	this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, true);
+	this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, this.texture.image);
+	this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+	this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+	// Clamp horizontally, but not vertically
+	this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT); 
+	this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
 
-	if (texture.loaded) {
-		gl.activeTexture(gl.TEXTURE0);
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.uniform1i(shader_program.unif_tex, 0);
+	this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+	this.texture.loaded = true;
+	this.draw_sphere();
+}
+
+Photosphere.prototype.draw_sphere = function()
+{
+	this.gl.viewport(0, 0, this.gl.viewport_width, this.gl.viewport_height);
+
+	if (this.texture.loaded) {
+		this.gl.activeTexture(this.gl.TEXTURE0);
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+		this.gl.uniform1i(this.shader_program.unif_tex, 0);
 	}
 
 	var camera_yaw_mat_raw = [
-		 Math.cos(camera_yaw), Math.sin(camera_yaw), 0,
-		-Math.sin(camera_yaw), Math.cos(camera_yaw), 0,
+		 Math.cos(this.camera_yaw), Math.sin(this.camera_yaw), 0,
+		-Math.sin(this.camera_yaw), Math.cos(this.camera_yaw), 0,
 		 0,                    0,                    1
 	]
 
 	var camera_pitch_mat_raw = [
 		1,  0,                      0,
-		0,  Math.cos(camera_pitch), Math.sin(camera_pitch),
-		0, -Math.sin(camera_pitch), Math.cos(camera_pitch)
+		0,  Math.cos(this.camera_pitch), Math.sin(this.camera_pitch),
+		0, -Math.sin(this.camera_pitch), Math.cos(this.camera_pitch)
 	]
 
-	var camera_mat_raw = multiply_matrices(camera_pitch_mat_raw, camera_yaw_mat_raw);
+	var camera_mat_raw = this.multiply_matrices(camera_pitch_mat_raw, camera_yaw_mat_raw);
 
 	var camera_rot = new Float32Array(camera_mat_raw);
 
-	var aspect = gl.viewport_width / gl.viewport_height;
+	var aspect = this.gl.viewport_width / this.gl.viewport_height;
 
-	var fov_y = camera_fov_y_degrees / 180 * Math.PI;
+	var fov_y = this.camera_fov_y_degrees / 180 * Math.PI;
 	var fov_x = 2 * Math.atan(aspect * Math.tan(fov_y / 2))
 
-	gl.uniform1f(shader_program.unif_fov_x, fov_x);
-	gl.uniform1f(shader_program.unif_fov_y, fov_y);
-	gl.uniformMatrix3fv(shader_program.unif_camera_rot, false, camera_rot);
+	this.gl.uniform1f(this.shader_program.unif_fov_x, fov_x);
+	this.gl.uniform1f(this.shader_program.unif_fov_y, fov_y);
+	this.gl.uniformMatrix3fv(this.shader_program.unif_camera_rot, false, camera_rot);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, vrt_pos_buf);
-	gl.vertexAttribPointer(shader_program.vrt_pos_attr, vrt_pos_buf.item_size, gl.FLOAT, false, 0, 0);
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, vrt_pos_buf.num_items);
+	this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vrt_pos_buf);
+	this.gl.vertexAttribPointer(this.shader_program.vrt_pos_attr, this.vrt_pos_buf.item_size, this.gl.FLOAT, false, 0, 0);
+	this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, this.vrt_pos_buf.num_items);
 }
 
-function show_photosphere(canvas_id, image_src)
+Photosphere.prototype.mouse_event_down = function(event)
 {
-	var canvas = document.getElementById(canvas_id);
-	if (!canvas) {
-		alert('Unable to find canvas with ID "' + canvas_id + '"!');
-		return;
+	if (event.button == 0) {
+		this.camera_rotating = true;
+		this.mouse_last_pos_x = event.screenX;
+		this.mouse_last_pos_y = event.screenY;
 	}
-
-	canvas.width = canvas.offsetWidth;
-	canvas.height = canvas.offsetHeight;
-
-	// Listen mouse dragging
-	canvas.addEventListener('mousedown', function(event){
-		if (event.button == 0) {
-			camera_rotating = true;
-			mouse_last_pos_x = event.screenX;
-			mouse_last_pos_y = event.screenY;
-		}
-	});	
-	canvas.addEventListener('mouseup', function(event){
-		if (event.button == 0) {
-			camera_rotating = false;
-		}
-	});
-	canvas.addEventListener('mousemove', function(event){
-		if ((event.buttons != undefined && event.buttons & 1) ||
-		    (event.buttons == undefined && camera_rotating)) {
-			var delta_x = event.screenX - mouse_last_pos_x;
-			var delta_y = event.screenY - mouse_last_pos_y;
-			mouse_last_pos_x = event.screenX;
-			mouse_last_pos_y = event.screenY;
-
-			var rot_speed = camera_fov_y_degrees / 35000;
-
-			camera_yaw += delta_x * rot_speed;
-			camera_pitch += delta_y * rot_speed;
-			if (camera_pitch > Math.PI / 2) {
-				camera_pitch = Math.PI / 2;
-			} else if (camera_pitch < -Math.PI / 2) {
-				camera_pitch = -Math.PI / 2;
-			}
-			draw_sphere();
-		}
-	});
-	// Listen mouse wheel
-	canvas.addEventListener('mousewheel', function(event){
-		camera_fov_y_degrees -= event.wheelDelta * 0.025;
-		if (camera_fov_y_degrees < 15) {
-			camera_fov_y_degrees = 15;
-		}
-		if (camera_fov_y_degrees > 90) {
-			camera_fov_y_degrees = 90;
-		}
-		draw_sphere();
-	});	
-	canvas.addEventListener('DOMMouseScroll', function(event){
-		camera_fov_y_degrees += event.detail;
-		if (camera_fov_y_degrees < 15) {
-			camera_fov_y_degrees = 15;
-		}
-		if (camera_fov_y_degrees > 90) {
-			camera_fov_y_degrees = 90;
-		}
-		draw_sphere();
-	});
-
-	init_gl(canvas);
-	shader_program = init_shaders();
-	init_buffers();
-	init_texture(image_src);
-
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-	draw_sphere();
 }
 
-function multiply_matrices(a, b)
+Photosphere.prototype.mouse_event_up = function(event)
+{
+	if (event.button == 0) {
+		this.camera_rotating = false;
+	}
+}
+
+Photosphere.prototype.mouse_event_move = function(event)
+{
+	if ((event.buttons != undefined && event.buttons & 1) ||
+	    (event.buttons == undefined && this.camera_rotating)) {
+		var delta_x = event.screenX - this.mouse_last_pos_x;
+		var delta_y = event.screenY - this.mouse_last_pos_y;
+		this.mouse_last_pos_x = event.screenX;
+		this.mouse_last_pos_y = event.screenY;
+
+		var rot_speed = this.camera_fov_y_degrees / 35000;
+
+		this.camera_yaw += delta_x * rot_speed;
+		this.camera_pitch += delta_y * rot_speed;
+		if (this.camera_pitch > Math.PI / 2) {
+			this.camera_pitch = Math.PI / 2;
+		} else if (this.camera_pitch < -Math.PI / 2) {
+			this.camera_pitch = -Math.PI / 2;
+		}
+		this.draw_sphere();
+	}
+}
+
+Photosphere.prototype.mouse_event_wheel = function(event)
+{
+	var value = event.wheelDelta;
+	if (value == undefined) {
+		value = -event.detail;
+	}
+	if (value > 0) {
+		this.camera_fov_y_degrees -= 3;
+		if (this.camera_fov_y_degrees < 15) {
+			this.camera_fov_y_degrees = 15;
+		}
+	} else if (value < 0) {
+		this.camera_fov_y_degrees += 3;
+		if (this.camera_fov_y_degrees > 90) {
+			this.camera_fov_y_degrees = 90;
+		}
+	}
+	this.draw_sphere();
+}
+
+Photosphere.prototype.multiply_matrices = function(a, b)
 {
 	return [
 		a[0]*b[0] + a[1]*b[3] + a[2]*b[6], a[0]*b[1] + a[1]*b[4] + a[2]*b[7], a[0]*b[2] + a[1]*b[5] + a[2]*b[8],
